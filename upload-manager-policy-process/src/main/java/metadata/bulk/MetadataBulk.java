d6b8b947c9b70f81d97e83354c0ae6ffcb2513d2
Ivano Cortesini <ivano.cortesini@gmail.com>
2016-05-04 17:16:32 +0200

    ...

---
+++ upload-manager-policy-process/src/main/java/metadata/bulk/MetadataBulk.java
@@ -0,0 +1,42 @@
+package metadata.bulk;
+
+import org.fao.ess.uploader.core.dto.ChunkMetadata;
+import org.fao.ess.uploader.core.dto.FileMetadata;
+import org.fao.ess.uploader.core.metadata.MetadataStorage;
+import org.fao.ess.uploader.core.process.PostUpload;
+import org.fao.ess.uploader.core.process.ProcessInfo;
+import org.fao.ess.uploader.core.storage.BinaryStorage;
+import sftp.SftpPropertiesValues;
+import sftp.SftpUpload;
+
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+
+@ProcessInfo(context = "policy.metadata.bulk", name = "PolicyData", priority = 1)
+public class MetadataBulk implements PostUpload {
+
+    @Override
+    public void chunkUploaded(ChunkMetadata metadata, MetadataStorage metadataStorage, BinaryStorage storage) throws Exception {
+        //Nothing to do here
+    }
+
+    @Override
+    public void fileUploaded(FileMetadata metadata, MetadataStorage metadataStorage, BinaryStorage storage, Map<String, Object> processingParams) throws Exception {
+        transferFile(
+                processingParams!=null ? (String)processingParams.get("policy") : null,
+                metadata.getName()!=null ? metadata.getName() : (String)processingParams.get("name"),
+                storage.readFile(metadata, null)
+        );
+    }
+
+
+    private void transferFile(String policyId, String fileName, InputStream dataStream) throws Exception {
+        //Main Properties file
+        SftpPropertiesValues properties = new SftpPropertiesValues();
+        Properties prop = properties.getPropValues('m');
+        //Transfer file
+        new SftpUpload().connect(prop, null, null, null, dataStream, fileName, policyId);
+    }
+    
+}
